7c3aef69243e47862734a1b7a071aacc8dafc01c
Pawel <p3jawors@gmail.com>
2021-07-08 21:37:11 -0400

    Started process for loading dataset for q2

--- train_RNN.py
+++ train_RNN.py
@@ -1,11 +1,32 @@
 # import required packages
+import csv
+import numpy as np
+
+with open('data/q2_dataset.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    x = []
+    xi = []
+    y = []
+    for ii, row in enumerate(reader):
+        if ii == 0:
+            continue
+        if ii%4 == 0:
+            y.append(np.copy(row))
+            x.append(np.copy(xi))
+            xi = []
+        else:
+            xi.append(row)
+        if ii == 10:
+            break
+print('x: ', x)
+print('y: ', y)
 
 
 # YOUR IMPLEMENTATION
 # Thoroughly comment your code to make it easy to follow
 
 
-if __name__ == "__main__": 
+# if __name__ == "__main__": 
 	# 1. load your training data
 
 	# 2. Train your network
