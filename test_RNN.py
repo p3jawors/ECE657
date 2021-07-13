47def1adff1f3f30727a5a058b7d40921ade77e6
Pawel <p3jawors@gmail.com>
2021-07-12 23:19:48 -0400

    Test plotting finished

--- test_RNN.py
+++ test_RNN.py
@@ -26,15 +26,25 @@ if __name__ == "__main__":
     score = model.evaluate(test_data, test_labels, batch_size=1, verbose=True)
 
     scores = []
+    predictions = []
     # cycle through each test point to get loss at each point
     for ii in range(0, test_data.shape[0]):
         # reshape to maintain dimensionality
         dat = np.expand_dims(test_data[ii], 0)
         lab = np.expand_dims(np.asarray(test_labels[ii]), 0)
-        scores.append(model.evaluate(dat, lab, verbose=verbose))
+        # scores.append(model.evaluate(dat, lab, verbose=verbose))
+        predictions.append(model.predict(dat, verbose=verbose))
+        scores.append(predictions[ii]-lab)
     plt.figure()
-    plt.title('RNN Test Loss')
-    plt.plot(scores)
+    plt.subplot(211)
+    plt.title('RNN Test Predictions')
+    plt.plot(np.squeeze(predictions), label='predicitons')
+    plt.plot(test_labels, label='ground truth')
+    plt.legend()
+    plt.subplot(212)
+    plt.title('RNN Prediction Difference')
+    plt.plot(np.squeeze(scores))
+    plt.legend()
     plt.show()
 
     #TODO need to extract predictions and plot them against GT
